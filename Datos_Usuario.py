f570101507cc2879df4d50a3addb3e41dfeb09c1
Matias <Mati@gmail.com>
2025-04-20 23:38:26 +0000

    Agrega script de Datos_Usuario.py con nombre y datos de estudiantes

---
+++ Datos_Usuario.py
@@ -0,0 +1,9 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+codigo = input("Ingrese su código-sección (ej: CS1102): ")
+sede = input("Ingrese su sede (ej: Santiago, Viña del Mar): ")
+
+print("\n--- Datos Ingresados ---")
+print(f"Nombre completo: {nombre} {apellido}")
+print(f"Código-sección: {codigo}")
+print(f"Sede: {sede}")
