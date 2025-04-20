d913be5378b3abb6b141410699ed05285dff51b2
Matias <Mati@gmail.com>
2025-04-20 22:26:47 +0000

    agrega script setup.sh

---
+++ Documentos/devasc_eval/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "Evaluacion_dry7122" > prueba.txt
+
+
+chmod 744 prueba.txt
+
+
+sudo chown root:root prueba.txt
+
+
+echo "¡Script ejecutado correctamente!"
+echo "Archivo creado: prueba.txt"
+echo "Permisos actualizados y propiedad asignada a root."
